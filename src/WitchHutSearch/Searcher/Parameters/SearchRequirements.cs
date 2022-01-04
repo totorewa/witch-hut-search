a3e8576736058ce0cf3ad3050820abf6dd99155d
Totorewa <totorewa@outlook.com>
2022-01-04 22:36:01 +1300

    Divide search range on Z axis
    The search goes north and then west. The main loop is on the X axis.
    Small optimisations are done so that the biome and swamp hut structure
    don't have to be regenerated for a region that it was already generated
    for in the previous iteration. However, this resets when moving up the Z
    axis. It is therefore more reasonable the divide the search range on the
    Z axis so that the X axis remains full width and the majority of the
    scan can make use of the optimisation and reset less.

--- src/WitchHutSearch/Searcher/Parameters/SearchRequirements.cs
+++ src/WitchHutSearch/Searcher/Parameters/SearchRequirements.cs
@@ -2,23 +2,23 @@
 
 public class SearchRequirements
 {
-    private int _count = (int)WitchHutCount.Quad;
-    public int Range { get; set; } = 500;
+    private readonly int _count = (int)WitchHutCount.Quad;
+    public int Range { get; init; } = 500;
 
     public int Count
     {
         get => _count;
-        set => _count = Math.Clamp(value, (int)WitchHutCount.Double, (int)WitchHutCount.Quad);
+        init => _count = Math.Clamp(value, (int)WitchHutCount.Double, (int)WitchHutCount.Quad);
     }
 
     public IEnumerable<SearchRange> CreateSearchRanges(int count)
     {
         var rangeSector = (int)Math.Ceiling((double)Range / count);
         var sectorSize = rangeSector * 2;
-        var minX = -(rangeSector * count);
-        var minZ = -Range;
-        var depthZ = Range * 2;
+        var minZ = -(rangeSector * count);
+        var minX = -Range;
+        var width = Range * 2;
         for (var i = 0; i < count; i++)
-            yield return new SearchRange(minX + sectorSize * i, minZ, sectorSize - (i == count - 1 ? 0 : 1), depthZ);
+            yield return new SearchRange(minX, minZ + sectorSize * i, width, sectorSize - (i == count - 1 ? 0 : 1));
     }
 }
